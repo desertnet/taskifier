99269c67f10ff00f4e3b25ef61a991e23d21c221
Eric McCarthy <eric@limulus.net>
2015-01-05 20:40:39 -0700

    Include In-Reply-To in references method return

--- test/EmailTest.js
+++ test/EmailTest.js
@@ -8,9 +8,10 @@ var Email = require("../lib/Email.js")
 
 describe("Email", function () {
   var email
-  var helloEmail = fs.readFileSync(path.join(__dirname, "data", "hello.eml"))
+  var helloEmail = testFileData("hello.eml")
   var helloEmailId = "<96A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>".toLowerCase()
-  var headerlessEmail = fs.readFileSync(path.join(__dirname, "data", "headerless.txt"))
+  var headerlessEmail = testFileData("headerless.txt")
+  var justReplyToEmail = testFileData("just-reply-to.eml")
 
   beforeEach(function () {
     email = new Email()
@@ -50,14 +51,28 @@ describe("Email", function () {
 
   describe("references()", function () {
     it("should return an array of message ids from the References header", function (done) {
-      email.initializeFromEmailText(helloEmail, function (err, helloEmail) {
+      email.initializeFromEmailText(helloEmail, function (err, email) {
         assert.ifError(err)
-        assert.deepEqual(helloEmail.references(), [
-          "<86A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>",
-          "<76A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>"
+        assert.deepEqual(email.references(), [
+          "<86A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>".toLowerCase(),
+          "<76A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>".toLowerCase()
+        ])
+        return done()
+      })
+    })
+
+    it("should include value from In-Reply-To header if no References header", function (done) {
+      email.initializeFromEmailText(justReplyToEmail, function (err) {
+        assert.ifError(err)
+        assert.deepEqual(email.references(), [
+          "<76A01A9E-582B-42EC-8B56-FC8E3CB57571@limulus.net>".toLowerCase()
         ])
         return done()
       })
     })
   })
 })
+
+function testFileData (name) {
+  return fs.readFileSync(path.join(__dirname, "data", name))
+}
