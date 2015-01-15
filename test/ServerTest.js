5fa30724f738bed227e8440edd0d28d698afe6de
Eric McCarthy <eric@limulus.net>
2015-01-15 14:45:03 -0700

    /inbox/{name}/email route

--- test/ServerTest.js
+++ test/ServerTest.js
@@ -3,8 +3,12 @@
 var assert = require("assert")
 var portfinder = require("portfinder")
 var request = require("request")
+var fs = require("fs")
+var path = require("path")
 
 var Server = require("../lib/Server.js")
+
+var helloEmailText = testFileData("hello.eml")
 
 describe("Server", function () {
   var server, port, addr, url
@@ -36,12 +40,61 @@ describe("Server", function () {
       statusUrl = url + "/status"
     })
 
-    it("should return 200 OK", function (done) {
-      request(statusUrl, function (err, res) {
-        assert.ifError(err)
-        assert.strictEqual(res.statusCode, 200)
-        return done()
+    describe("GET", function () {
+      it("should return 200 OK", function (done) {
+        request(statusUrl, function (err, res) {
+          assert.ifError(err)
+          assert.strictEqual(res.statusCode, 200)
+          return done()
+        })
+      })
+    })
+  })
+
+  describe("/inbox/support/email", function () {
+    var inboxEmailUrl, taskGenerator
+
+    before(function () {
+      inboxEmailUrl = url + "/inbox/support/email"
+    })
+
+    beforeEach(function () {
+      taskGenerator = new TaskGeneratorMock()
+      server.setTaskGeneratorForInbox("support", taskGenerator)
+    })
+
+    describe("POST", function () {
+      it("should taskify the email", function (done) {
+        var req = {
+          "method": "POST",
+          "url": inboxEmailUrl,
+          "body": helloEmailText
+        }
+        request(req, function (err, res) {
+          assert.ifError(err)
+          assert.strictEqual(res.statusCode, 200, res.body)
+          taskGenerator.assertTaskifyEmailWasCalledCorrectly()
+          return done()
+        })
       })
     })
   })
 })
+
+function TaskGeneratorMock () {
+  this._taskifyEmailCalled = false
+}
+
+TaskGeneratorMock.prototype.taskifyEmail = function (email, cb) {
+  this._taskifyEmailCalled = true
+  assert.strictEqual(email.raw(), helloEmailText.toString())
+  return cb(undefined)
+}
+
+TaskGeneratorMock.prototype.assertTaskifyEmailWasCalledCorrectly = function () {
+  assert.ok(this._taskifyEmailCalled, "taskifyEmail wasn't called")
+}
+
+function testFileData (name) {
+  return fs.readFileSync(path.join(__dirname, "data", name))
+}
