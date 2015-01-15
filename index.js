7c026d24bf44ab0a5fbbdf79759895a1654a8031
Eric McCarthy <eric@limulus.net>
2015-01-15 16:00:01 -0700

    The taskifier binary for starting a server

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict"
+
+module.exports.Database = require("./lib/Database.js")
+module.exports.EmailImporter = require("./lib/EmailImporter.js")
+module.exports.Server = require("./lib/Server.js")
